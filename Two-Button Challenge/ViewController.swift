ee45a20ac2a36067135ccacc09602037fb65d25b
Rhett Somers <somersr@bc.edu>
2022-02-02 16:51:24 -0500

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Rhett Somers on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
