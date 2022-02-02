bc1c29c317b9447fcf0d9d404212b8ae15826626
Rhett Somers <somersr@bc.edu>
2022-02-02 16:58:40 -0500

    2.3 - Finished Two-Button Challenge and Changed Text Color

--- Two-Button Challenge/ViewController.swift
+++ Two-Button Challenge/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showMessageButton(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+        messageLabel.textColor = .blue
+    }
+    @IBAction func showAnotherMessageButton(_ sender: UIButton) {
+        messageLabel.text = "You Are Great!"
+        messageLabel.textColor = .systemRed
+    }
+    
 
 }
 
